e66cf91170c0e11bae90c251716b9d76d80facb3
Mohamad Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2025-03-25 01:32:35 +0300

    Create AES_ECB_chosen-plaintext-attack-prefix-miniboss.py

---
+++ Cryptography/AES_ECB_chosen-plaintext-attack-prefix-miniboss.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+from pwn import *
+
+context.arch = 'amd64'
+
+challenge_file = "CHALLENGE_FILE"
+ps = process(challenge_file)
+GREEN = "\033[92m"
+RED = "\033[95m"
+RESET = "\033[0m"
+
+# pt = bytes.fromhex(input("Data? ").strip()) + flag
+# ct = cipher.encrypt(pad(pt, cipher.block_size))
+# print(f"Ciphertext: {ct.hex()}")
+
+def main():
+    flag = ''
+    print("\n")
+
+    ## The 5th block starts showing up at the 7th character, but you need to account for the padding block so start from the 8th one.
+    for counter in range(8, 65):
+        # Loop through ASCII printable chars.
+        for ascii_value in range(33, 127):
+            block_size = 16
+            prefix = b'A' * counter
+
+            if len(flag) < 16:
+                fragment = chr(ascii_value) + flag
+            
+            else:
+                fragment = chr(ascii_value) + flag[0:15]
+
+            pad_len = block_size - len(fragment)
+            my_input = fragment.encode() + (bytes([pad_len]) * pad_len) + prefix
+            my_hex_input = my_input.hex()
+            ps.sendlineafter(b"Data? ", my_hex_input.encode())
+
+            ## Receive the encrypted flag.
+            line = ps.recvline().strip()
+            hex_temp_fragment = line.split(b'Ciphertext: ')[-1].decode().strip()
+
+            blocks = [hex_temp_fragment[i:i+32] for i in range(0, len(hex_temp_fragment), 32)]
+            if blocks[0] == blocks[5]:
+                print(f"{RED}Found Character: {chr(ascii_value)}{RESET}")
+                flag = chr(ascii_value) + flag
+
+            # formatted = '     '.join(hex_temp_fragment[i:i+32] for i in range(0, len(hex_temp_fragment), 32))
+            # print(f"{formatted}:{counter}")
+    
+    print(f"\n{GREEN}{flag}{RESET}\n")
+
+if __name__ == "__main__":
+    main()
